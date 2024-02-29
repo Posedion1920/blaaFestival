890b850865235893c44d1648c4734277799ece13
Mohamed <57219435+SeenDGK@users.noreply.github.com>
2024-02-29 14:52:03 +0100

    3eqwewqeqwe

--- assets/js/praktiskInfo.js
+++ assets/js/praktiskInfo.js
@@ -16,11 +16,16 @@ accordionItems.forEach(item => {
 const BurgermenuEl = document.querySelector(".burger-menu");
 const menuEl = document.querySelector(".menu");
 const xmarkEl = document.getElementById("Remove")
+const Btn = document.querySelector(".LiveButton");
+const dot = document.querySelector(".redDOt");
 
 function ShowMenu() {
   menuEl.style.display = "block";
   BurgermenuEl.style.display = "none";
-}
+  Btn.style.backgroundColor="#FFFFFF"
+  Btn.style.color = "#DE851D";
+  dot.style.top="10px";
+};
 
 function RemoveMenu() {
   menuEl.style.display = "none";
@@ -29,9 +34,9 @@ function RemoveMenu() {
 
 BurgermenuEl.addEventListener("click", function () {
   ShowMenu();
-})
+});
 
 xmarkEl.addEventListener("click", function () {
   RemoveMenu();
-})
+});
 
